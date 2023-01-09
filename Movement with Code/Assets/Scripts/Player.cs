40f9d060a5828d51b2213060564d6d58d13d1cb0
Joshua <tank158r5155@yahoo.com.tw>
2023-01-09 15:56:28 +0800

    Initial commit

---
+++ Movement with Code/Assets/Scripts/Player.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    float walkSpeed = 4f;
+    float inputHorizontal;
+    float inputVertical;
+    float lastInputHorizontal;
+    float lastInputVertical;
+
+    // Animator and states
+    Animator animator;
+    string currentState;
+
+    // Movement animations
+    const string PLAYER_MOVE_LEFT = "Player_Move_Left";
+    const string PLAYER_MOVE_RIGHT = "Player_Move_Right";
+    const string PLAYER_MOVE_UP = "Player_Move_Up";
+    const string PLAYER_MOVE_DOWN = "Player_Move_Down";
+
+    // Idle animations
+    const string PLAYER_IDLE_LEFT = "Player_Idle_Left";
+    const string PLAYER_IDLE_RIGHT = "Player_Idle_Right";
+    const string PLAYER_IDLE_BACK = "Player_Idle_Back";
+    const string PLAYER_IDLE_FRONT = "Player_Idle_Front";
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+
+        // Set the default state to idle animation so the player
+        // won't start without an idle animation
+        ChangeAnimationState(PLAYER_IDLE_FRONT);
+    }
+
+    void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if (inputHorizontal != 0 || inputVertical != 0)
+        {
+            rb.velocity = new Vector2(inputHorizontal, inputVertical).normalized * walkSpeed;
+            lastInputHorizontal = inputHorizontal;
+            lastInputVertical = inputVertical;
+
+            if (inputHorizontal > 0)
+            {
+                ChangeAnimationState(PLAYER_MOVE_RIGHT);
+            }
+            else if (inputHorizontal < 0)
+            {
+                ChangeAnimationState(PLAYER_MOVE_LEFT);
+            }
+            else if (inputVertical > 0)
+            {
+                ChangeAnimationState(PLAYER_MOVE_UP);
+            }
+            else if (inputVertical < 0)
+            {
+                ChangeAnimationState(PLAYER_MOVE_DOWN);
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(0f, 0f);
+            if (lastInputHorizontal > 0)
+            {
+                ChangeAnimationState(PLAYER_IDLE_RIGHT);
+            }
+            else if (lastInputHorizontal < 0)
+            {
+                ChangeAnimationState(PLAYER_IDLE_LEFT);
+            }
+            else if (lastInputVertical > 0)
+            {
+                ChangeAnimationState(PLAYER_IDLE_BACK);
+            }
+            else if (lastInputVertical < 0)
+            {
+                ChangeAnimationState(PLAYER_IDLE_FRONT);
+            }
+
+        }
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+
+        animator.Play(newState);
+        currentState = newState;
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Top down game movement in Unity.
+
+### What is this?
+
+This is a fast prototype of using a same sprite asset to create a simple 2D top down movement with Unity.
+
+### Why?
+
+While I'm in the process of learning Game development with Unity, I've noticed that Top-down game movement can be achieved mainly in two ways: one with Unity's Blend Trees and the other with scripting. (There might be more ways to do so as I said I'm still learning game development :)) So I'm creating this little project in hope to find out best way to work with 2D top-down game movement
+
+### The tutorials
+
+I basically followed [this](https://www.youtube.com/watch?v=_ijgw2i0mmi&ab_channel=danikrossing) tutorial for player movement only thur code and [this one](https://www.youtube.com/watch?v=fRpoE4FfJf8&ab_channel=JTAGames) for using Blend Trees only.
+
+> There are more tutorials like this available on YT. You can find out more [here](https://github.com/konekoya/game-dev-links#2d-movement-and-animation)
+
+The asses I used in both tutorial is a free asset and is available [here](https://limezu.itch.io/moderninteriors). But you probably won't need to download that as the asset is already imported in the Unity editor
